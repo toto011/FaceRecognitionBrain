0db364224deff8956265e89d7bae4011c11bf993
Vuk Milicevic <vukmilicevic@mail.ru>
2019-01-18 23:53:01 +0100

    1st commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {''}
+            </div>
+            <div className='white f1'>
+                {''}
+            </div>
+
+        </div>
+    );
+}
+
+export default Rank;
